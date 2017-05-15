184a0c588debe7c0560d44e0186114ee39bfa8fb
NadiaKhomenko <nadja.chomenko@gmail.com>
2017-05-15 16:05:49 +0300

    add lipstick

---
+++ Purse3/Lipstick/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lipstick
+{
+    public class Lipstick
+    {
+        private bool Existance = false;
+        public string CheckExistance()
+        {
+            return "Lipstick existance: " + Existance.ToString();
+        }
+        public Lipstick(bool Existance)
+        {
+            this.Existance = Existance;
+        }
+
+    }
+}
