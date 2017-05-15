3c7e370be20669ede2dd58addc4a8ac615caad14
annamariias95 <ann.baleha864@gmail.com>
2017-05-15 15:32:56 +0300

    Add Credit Card

---
+++ Purse3/CreditCard/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreditCard
+{
+    public class CreditCard
+    {
+        public decimal amount = 0;
+        private string msg = "Mennyi penzed van? Szeretlek ";
+
+        public string GetSumCreditCard()
+        {
+            return msg + amount.ToString();
+        }
+
+        public CreditCard(decimal amount)
+        {
+            this.amount = amount;
+        }
+    }
+}
